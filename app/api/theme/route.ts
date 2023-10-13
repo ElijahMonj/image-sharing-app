91ebbe5a75fdc37ca3b8a73ee85cdb2df3eca0bd
Elijah Monjardin <monjardinelijah120@gmail.com>
2023-10-13 08:47:16 +0800

    Fixed theme to local to cloud

---
+++ app/api/theme/route.ts
@@ -0,0 +1,23 @@
+import prisma from "@/app/libs/prismadb"
+import { NextResponse } from "next/server"
+export async function POST(request:Request){
+    
+    try {
+        
+        const body=await request.json();
+        const {id,theme}=body;
+        const updatedTheme = await prisma.account.update({
+            where: {
+                id: id,
+            },
+            data: {
+              theme: theme,
+            },
+          })
+        return NextResponse.json(updatedTheme);
+        
+    } catch (error) {
+        console.log(error,'POST_ERROR')
+        return new NextResponse('Internal Error', {status:500});
+    }
+}
