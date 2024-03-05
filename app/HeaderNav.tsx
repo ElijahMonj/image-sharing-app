3c8662a660ca7423016db663983ef086201906c8
Elijah Monjardin <monjardinelijah120@gmail.com>
2024-03-05 08:19:34 +0800

    Changed id to email

--- app/HeaderNav.tsx
+++ app/HeaderNav.tsx
@@ -1,15 +1,18 @@
 import NavigationBar from "./Components/NavigationBar";
 import getCurrentUser from "./actions/getCurrentUser";
 import prisma from "@/app/libs/prismadb";
-
+import {getServerSession} from 'next-auth';
+import { authOptions } from '@/app/authOptions'
         
 const HeaderNav = async () => {
-    
-    const currentUser = await getCurrentUser();
+    const session = await getServerSession(authOptions);
+    if(!session?.user?.email){
+        console.log("ELIJAH NULL")
+        return null;
+    }
     const user = await prisma.user.findUnique({
         where: {
-            //@ts-ignore
-           id:currentUser.id
+            email: session.user.email as string
         }
     })
     
